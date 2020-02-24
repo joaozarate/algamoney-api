7720171ee2ecfea2458e9ab2129ff82dd1fe18ee
joaozarate <joaozarate@gmail.com>
2020-02-24 23:16:06 +0000

    Created method find Categoria by id

--- src/main/java/com/zarate/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/zarate/algamoney/api/resource/CategoriaResource.java
@@ -3,17 +3,15 @@ package com.zarate.algamoney.api.resource;
 import java.net.URI;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,7 +22,8 @@ import com.zarate.algamoney.api.repository.CategoriaRepository;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
-	private static final String LOCATION = "Location";
+//	private static final String LOCATION = "Location";
+
 	@Autowired
 	private CategoriaRepository repository;
 
@@ -45,6 +44,11 @@ public class CategoriaResource {
 
 		return ResponseEntity.created(uri).body(categoriaCriada);
 
+	}
+
+	@GetMapping("/{codigo}")
+	public Categoria buscarByCodigo(@PathVariable Long codigo) {
+		return repository.findById(codigo).orElse(null);
 	}
 
 }
