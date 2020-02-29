55ae06f40472f8d462228083ca29cf9badefd52b
joaozarate <joaozarate@gmail.com>
2020-02-29 13:06:56 +0000

    Delete categoria

--- src/main/java/com/zarate/algamoney/api/handler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/zarate/algamoney/api/handler/AlgamoneyExceptionHandler.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +17,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.zarate.algamoney.api.handler.AlgamoneyExceptionHandler.Erro;
 
 @ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
@@ -62,6 +62,16 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
 		return erros;
 
+	}
+
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(RuntimeException ex, WebRequest request) {
+
+		String userMessage = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		List<Erro> erros = Arrays.asList(new Erro(userMessage, ex.toString()));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		
 	}
 
 	public static class Erro implements Serializable {
--- src/main/java/com/zarate/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/zarate/algamoney/api/resource/CategoriaResource.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zarate.algamoney.api.event.RecursoCriadoEvent;
@@ -53,6 +55,12 @@ public class CategoriaResource {
 //				categoria -> ResponseEntity.ok(categoria)).orElse(ResponseEntity.notFound().build());
 		Optional<Categoria> categoria = repository.findById(codigo);
 		return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		repository.deleteById(codigo);
 	}
 
 }
