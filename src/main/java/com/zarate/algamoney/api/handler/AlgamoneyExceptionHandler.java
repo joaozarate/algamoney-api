5d7bd1a5df2189b63233c7833e4df5a62fa1bf80
joaozarate <joaozarate@gmail.com>
2020-03-02 19:43:57 +0000

    validating inconsistencies

--- src/main/java/com/zarate/algamoney/api/handler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/zarate/algamoney/api/handler/AlgamoneyExceptionHandler.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -33,7 +35,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-		String mensagemDev = ex.getCause() != null ? ex.getCause().toString() : ex.toString();
+		String mensagemDev = ExceptionUtils.getRootCauseMessage(ex);
 
 		return super.handleExceptionInternal(ex, Arrays.asList(new Erro(mensagemUsuario, mensagemDev)), headers,
 				HttpStatus.BAD_REQUEST, request);
@@ -43,19 +45,19 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-		List<Erro> erros = criarErros(ex.getBindingResult());
-
+		List<Erro> erros = criarErros(ex.getBindingResult(), ex);
 		return super.handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+
 	}
 
-	private List<Erro> criarErros(BindingResult bindingResult) {
+	private List<Erro> criarErros(BindingResult bindingResult, Throwable ex) {
 
 		List<Erro> erros = new ArrayList<Erro>();
 
 		for (FieldError erro : bindingResult.getFieldErrors()) {
 
 			String msgUsuario = messageSource.getMessage(erro, LocaleContextHolder.getLocale());
-			String msgDev = erro.toString();
+			String msgDev = ExceptionUtils.getRootCauseMessage(ex);
 
 			erros.add(new Erro(msgUsuario, msgDev));
 		}
@@ -69,9 +71,20 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<Object> handleEmptyResultDataAccessException(RuntimeException ex, WebRequest request) {
 
 		String userMessage = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
-		List<Erro> erros = Arrays.asList(new Erro(userMessage, ex.toString()));
+		List<Erro> erros = Arrays.asList(new Erro(userMessage, ExceptionUtils.getRootCauseMessage(ex)));
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-		
+
+	}
+
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<Object> handlerDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+
+		String userMessage = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(userMessage, devMessage));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
 	}
 
 	public static class Erro implements Serializable {
